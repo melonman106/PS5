6e14d1df18f6036ec8387eb9529bd2fc843150de
melonman106 <102203577+melonman106@users.noreply.github.com>
2026-03-10 18:14:59 +1030

    Create vids.js

---
+++ vids.js
@@ -0,0 +1,16 @@
+window.videoDatabase = {
+    'vid1': { 
+        name: 'Venom', 
+        tag: 'Embrace your inner anti-hero', 
+        icon: 'img/v.png', 
+        hero: 'img/v1.png', 
+        file: 'vids/venom.mp4' 
+    },
+    'vid2': { 
+        name: 'Venom Let There Be Carnage', 
+        tag: 'Embrace your inner carnage', 
+        icon: 'img/vl.png', 
+        hero: 'img/vl1.png', 
+        file: 'vids/Venom2.mp4' 
+    }
+};
